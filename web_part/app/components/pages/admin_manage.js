00ed7fb765ed15c7691e9905c1ceb0861bb8f563
Мария Распопова <maryaraspopova@gmail.com>
2018-01-22 23:55:12 +0300

    впилены табы

--- web_part/app/components/pages/admin_manage.js
+++ web_part/app/components/pages/admin_manage.js
@@ -8,7 +8,10 @@ import { connect } from 'react-redux';
 import {getClientsInfo, getBrandsInfo, addBrand, deleteBrand, getBoxesInfo,
             addBox, deleteBox, incPriceBox, getFreeBoxesInfo} from '../../actions' ;
 
-import {Button, Container, Input, Table, FormGroup, Label } from "reactstrap";
+import {
+    Button, Container, Input, Table, FormGroup, Label, Row, Col, Card, CardTitle, CardText, Nav,
+    NavItem, NavLink, TabContent, TabPane
+} from "reactstrap";
 
 
 @connect(mapStateToProps)
@@ -25,8 +28,10 @@ export default class AdminManage extends Component{
             incPrice: null, //во сколько поднять цену
             addCarBrand: null, //добавляемая марку машины
             deleteCarBrand: null, //удаляемая марку
+            activeTab: '1'
         };
 
+        this.toggle = this.toggle.bind(this);
         this.onChangeSelectCarBrands = this.onChangeSelectCarBrands.bind(this);
         this.onSelectIncPriceNumberBox = this.onSelectIncPriceNumberBox.bind(this);
         this.onChangePriceBox = this.onChangePriceBox.bind(this);
@@ -46,7 +51,7 @@ export default class AdminManage extends Component{
         let p1 = this.props.dispatch(getFreeBoxesInfo());
         p.then(() => {
             let p2 = this.props.dispatch(getBrandsInfo());
-            /*p2.then(() => {
+            p2.then(() => {
                 console.log('componentDidMount', this.props);
                 let obj = {};
                 if (this.props.carBrandsById.length !== 0) {
@@ -58,11 +63,11 @@ export default class AdminManage extends Component{
                     obj['incPriceNumberBox'] = this.props.boxesList[0].id;
                 }
                 this.setState(obj);
-            })*/
+            })
         })
     }
 
-    componentWillReceiveProps(){
+    /*componentWillReceiveProps(){
         this.setState(function(prevState, props) {
             console.log('componentDidMount', this.props);
             let obj = {};
@@ -76,7 +81,7 @@ export default class AdminManage extends Component{
             }
             return obj;
         });
-    }
+    }*/
 
     onSelectIncPriceNumberBox(value) {
         this.setState({
@@ -139,132 +144,165 @@ export default class AdminManage extends Component{
     deleteCarBrand(){
         this.props.dispatch(deleteBrand(this.state.deleteCarBrand))
     }
+
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
+
     //{}
     render(){
         console.log('render', this.props, this.state);
         return(
             <Container>
-                <h2>
-                    Боксы
-                </h2>
-                <div>
-                    <div>
-                        <h3>Прием в эксплуатацию нового бокса</h3>
-                        <FormGroup>
-                            <Label>Название марки</Label>
-                            <Input type="select" onChange={(e) => {this.onChangeSelectCarBrands(e.target.value)}} value={this.state.selectCarBrand}>
-                                {
-                                    this.props.carBrandsById.map((brand, index)=>{
-                                        return (
-                                            <option value={brand.id} key={index}>
-                                                {brand.name}
-                                            </option>
-                                        )
-                                    })
-                                }
-                            </Input>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label>Цена</Label>
-                            <Input value={this.state.priceBox} onChange={(e) => {this.onChangePriceBox(e.target.value)}}/>
-                        </FormGroup>
-                        <Button color="primary" onClick={this.addNewBox}>Принять новый бокс</Button>
-                    </div>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={{ active: this.state.activeTab === '1' }}
+                            onClick={() => { this.toggle('1'); }}
+                        >
+                            Боксы
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={{ active: this.state.activeTab === '2' }}
+                            onClick={() => { this.toggle('2'); }}
+                        >
+                            Moar Tabs
+                        </NavLink>
+                    </NavItem>
+                </Nav>
 
-                    <hr/>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <h2>
+                            Боксы
+                        </h2>
+                        <div>
+                            <div>
+                                <h3>Прием в эксплуатацию нового бокса</h3>
+                                <FormGroup>
+                                    <Label>Название марки</Label>
+                                    <Input type="select" onChange={(e) => {this.onChangeSelectCarBrands(e.target.value)}} value={this.state.selectCarBrand}>
+                                        {
+                                            this.props.carBrandsById.map((brand, index)=>{
+                                                return (
+                                                    <option value={brand.id} key={index}>
+                                                        {brand.name}
+                                                    </option>
+                                                )
+                                            })
+                                        }
+                                    </Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Цена</Label>
+                                    <Input value={this.state.priceBox} onChange={(e) => {this.onChangePriceBox(e.target.value)}}/>
+                                </FormGroup>
+                                <Button color="primary" onClick={this.addNewBox}>Принять новый бокс</Button>
+                            </div>
 
-                    <div>
-                        <h3>Закрытие бокса</h3>
-                        <FormGroup>
-                            <Label>Номер бокса</Label>
-                            <Input type="select"
-                                onChange={(e) => {this.onSelectCloseNumberBox(e.target.value)}}
-                                value={this.state.closeNumberBox}>
-                                {
-                                    this.props.freeBoxesById.map((box, index)=>{
-                                        return (
-                                            <option value={box.id} key={index}>
-                                                {box.id}
-                                            </option>
-                                        )
-                                    })
-                                }
-                            </Input>
-                        </FormGroup>
-                        <Button color="primary" onClick={this.closeBox}>Закрыть бокс</Button>
-                    </div>
+                            <hr/>
 
-                    <hr/>
+                            <div>
+                                <h3>Закрытие бокса</h3>
+                                <FormGroup>
+                                    <Label>Номер бокса</Label>
+                                    <Input type="select"
+                                           onChange={(e) => {this.onSelectCloseNumberBox(e.target.value)}}
+                                           value={this.state.closeNumberBox}>
+                                        {
+                                            this.props.freeBoxesById.map((box, index)=>{
+                                                return (
+                                                    <option value={box.id} key={index}>
+                                                        {box.id}
+                                                    </option>
+                                                )
+                                            })
+                                        }
+                                    </Input>
+                                </FormGroup>
+                                <Button color="primary" onClick={this.closeBox}>Закрыть бокс</Button>
+                            </div>
 
-                    <div>
-                        <h3>Увеличить стоимость аренды</h3>
-                        <FormGroup>
-                            <Label>Номер бокса</Label>
-                            <Input type="select" onChange={(e) => {this.onSelectIncPriceNumberBox(e.target.value)}}
-                                    value={this.state.incPriceNumberBox}>
-                                {
-                                    this.props.boxesList.map((box, index)=>{
-                                        return (
-                                            <option value={box.id} key={index}>
-                                                {box.id}
-                                            </option>
-                                        )
-                                    })
-                                }
-                            </Input>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label>Во сколько раз</Label>
-                            <Input value={this.state.incPrice} onChange={(e) => {this.onChangeIncPrice(e.target.value)}}/>
-                        </FormGroup>
-                        <Button color="primary" onClick={this.incPriceBox}>Увеличить стоимость боксов</Button>
-                    </div>
-                </div>
+                            <hr/>
 
-                <hr/>
-
-                <h2>
-                    Марки
-                </h2>
-
-                <div>
-                    <div>
-                        <h3>Добавить марку в перечень</h3>
-                        <FormGroup>
-                            <Label>Название марки</Label>
-                            <Input value={this.state.addCarBrand} onChange={(e) => {this.onChangeAddCarBrand(e.target.value)}}/>
-                        </FormGroup>
-                        <Button color="primary" onClick={this.addCarBrand}>Добавить марку</Button>
-                    </div>
-
-                    <hr/>
-
-                    <div>
-                        <h3>Удалить марку</h3>
-                        <FormGroup>
-                            <Label>Название марки</Label>
-                            <Input type="select" onChange={(e) => {this.onChangeDeleteCarBrand(e.target.value)}}
-                                value={this.state.deleteCarBrand}>
-                                {
-                                    this.props.carBrandsById.map((brand, index)=>{
-                                        return (
-                                            <option value={brand.id} key={index}>
-                                                {brand.name}
-                                            </option>
-                                        )
-                                    })
-                                }
-                            </Input>
-                        </FormGroup>
-                        <Button color="primary" onClick={this.deleteCarBrand}>Удалить марку</Button>
-                    </div>
-                </div>
+                            <div>
+                                <h3>Увеличить стоимость аренды</h3>
+                                <FormGroup>
+                                    <Label>Номер бокса</Label>
+                                    <Input type="select" onChange={(e) => {this.onSelectIncPriceNumberBox(e.target.value)}}
+                                           value={this.state.incPriceNumberBox}>
+                                        {
+                                            this.props.boxesList.map((box, index)=>{
+                                                return (
+                                                    <option value={box.id} key={index}>
+                                                        {box.id}
+                                                    </option>
+                                                )
+                                            })
+                                        }
+                                    </Input>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Во сколько раз</Label>
+                                    <Input value={this.state.incPrice} onChange={(e) => {this.onChangeIncPrice(e.target.value)}}/>
+                                </FormGroup>
+                                <Button color="primary" onClick={this.incPriceBox}>Увеличить стоимость боксов</Button>
+                            </div>
+                        </div>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <h2>Марки</h2>
+                        <Row>
+                            <Col sm="6">
+                                <Card body>
+                                    <CardTitle>Добавить марку в перечень</CardTitle>
+                                    <FormGroup>
+                                        <Label>Название марки</Label>
+                                        <Input value={this.state.addCarBrand} onChange={(e) => {this.onChangeAddCarBrand(e.target.value)}}/>
+                                    </FormGroup>
+                                    <Button color="primary" onClick={this.addCarBrand}>Добавить марку</Button>
+                                </Card>
+                            </Col>
+                            <Col sm="6">
+                                <Card body>
+                                    <CardTitle>Удалить марку</CardTitle>
+                                    <CardText>
+                                        <FormGroup>
+                                            <Label>Название марки</Label>
+                                            <Input type="select" onChange={(e) => {this.onChangeDeleteCarBrand(e.target.value)}}
+                                                   value={this.state.deleteCarBrand}>
+                                                {
+                                                    this.props.carBrandsById.map((brand, index)=>{
+                                                        return (
+                                                            <option value={brand.id} key={index}>
+                                                                {brand.name}
+                                                            </option>
+                                                        )
+                                                    })
+                                                }
+                                            </Input>
+                                        </FormGroup>
+                                    </CardText>
+                                    <Button color="primary" onClick={this.deleteCarBrand}>Удалить марку</Button>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
             </Container>
         )
     }
 }
 
 function mapStateToProps(state, ownProps) {
+    let rents = state.client.clientListTicket;
+
     console.log(state);
     return {
         carBrandsById: state.brands.brandsList,
